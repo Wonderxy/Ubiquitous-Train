9d4b091a78c462e0b2cad4be2725a91aa601d9a2
Wonderxy <1461934988@qq.com>
2023-11-28 09:06:19 +0800

    update

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import tensorly as tl
 import sys
@@ -16,22 +15,3 @@ if __name__ == '__main__':
     fit = tm.fit(ttList[0].to_tensor(),tList)
     print(fit)
 
-=======
-import numpy as np
-import tensorly as tl
-import sys
-sys.path.append('d:\\Files\\VisualStudioCode\\TT2.0\\Ubiquitous-Train')
-import tensor.mathematical as tm
-import os
-import datetime
-import storage.ttt_access as st
-
-path = 'D:/Files/VisualStudioCode/TT2.0/datastorage/'
-
-if __name__ == '__main__':
-    ttList = st.load_tt([f"ttResult-3-2-1000-0"],path+"tt_storage/")
-    tList = st.load_tensor([f"tResult-3-2-1000"],path+"t_storage/")
-    fit = tm.fit(ttList[0].to_tensor(),tList)
-    print(fit)
-    #test
->>>>>>> 151d07fbfed812cbd727102511831834e09439b0
